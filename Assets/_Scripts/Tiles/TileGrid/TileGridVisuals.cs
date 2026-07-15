0c8acd22072c654967453063cf2a7a4008c60b59
MaeMei-Maria <140622816+MaeMei-Maria@users.noreply.github.com>
2026-07-15 02:29:10 +0200

    .

--- Assets/_Scripts/Tiles/TileGrid/TileGridVisuals.cs
+++ Assets/_Scripts/Tiles/TileGrid/TileGridVisuals.cs
@@ -4,8 +4,6 @@ using _Scripts.Core.Mediator;
 using _Scripts.Occupants;
 using UnityEngine;
 using UnityEngine.Pool;
-using UnityEngine.Tilemaps;
-using UnityEngine.Timeline;
 
 namespace _Scripts.Tiles.TileGrid
 {
@@ -17,32 +15,45 @@ namespace _Scripts.Tiles.TileGrid
         private ObjectPool<GameObject> _actionTileMarkerPool;
 
         private readonly List<GameObject> _actionTileMarkers = new();
-        
-        private static readonly Color OccupantColor = new Color(1f, 1f, 0f, 0.75f); 
-        private static readonly Color InteractorColor = new Color(0f, 0.5f, 1f, 0.75f); 
-        private static readonly Color DefaultColor = new Color(1f, 1f, 1f, 0.75f);
-        
+
+        private static readonly Color OccupantColor = new(1f, 1f, 0f, 0.75f);
+        private static readonly Color InteractorColor = new(0f, 0.5f, 1f, 0.75f);
+        private static readonly Color DefaultColor = new(1f, 1f, 1f, 0.75f);
+
         protected override void Awake()
         {
             base.Awake();
+
             _actionTileMarkerPool = new ObjectPool<GameObject>(OnCreateMarker, OnGetMarker, OnReleaseMarker);
         }
 
         private GameObject OnCreateMarker()
         {
-            var markerCopy = Instantiate(actionMarkerPrefab, Vector3.zero, Quaternion.identity);
-            _actionTileMarkers.Add(markerCopy);
-            return markerCopy;
+            Debug.Log("CREATE");
+
+            var marker = Instantiate(actionMarkerPrefab, Vector3.zero, Quaternion.identity);
+
+            _actionTileMarkers.Add(marker);
+
+            Debug.Log($"CREATE -> {marker.GetInstanceID()} Pos:{marker.transform.position}");
+
+            return marker;
         }
-        
+
         private void OnGetMarker(GameObject marker)
         {
-            marker.gameObject.SetActive(true);
+            Debug.Log($"GET BEFORE -> {marker.GetInstanceID()} Pos:{marker.transform.position}");
+
+            marker.SetActive(true);
+
+            Debug.Log($"GET AFTER -> {marker.GetInstanceID()} Pos:{marker.transform.position}");
         }
-        
+
         private void OnReleaseMarker(GameObject marker)
         {
-            marker.gameObject.SetActive(false);
+            Debug.Log($"RELEASE -> {marker.GetInstanceID()} Pos:{marker.transform.position}");
+
+            marker.SetActive(false);
         }
 
         private void OnEnable()
@@ -51,29 +62,45 @@ namespace _Scripts.Tiles.TileGrid
             mediator.OnTileClicked += HandleOnTileClicked;
         }
 
+        private void OnDisable()
+        {
+            mediator.OnMovementTilesSet -= HighlightMovementTiles;
+            mediator.OnTileClicked -= HandleOnTileClicked;
+        }
+
         private void HighlightMovementTiles(IEnumerable<LevelTile> movementTiles)
         {
+            Debug.Log("===== HighlightMovementTiles =====");
+
             HideHighlightTiles();
-            var tilesHighlighted = movementTiles as LevelTile[] ?? movementTiles.ToArray();
-            
-            foreach (var tile in tilesHighlighted)
+
+            foreach (var tile in movementTiles)
             {
+                Debug.Log($"Tile: {tile.WorldPosition}");
+
                 var marker = _actionTileMarkerPool.Get();
-                marker.transform.position = new Vector3(
-                    tile.WorldPosition.x + tile.TilemapMember.cellSize.x * 0.5f,
-                    tile.WorldPosition.y +tile.TilemapMember.cellSize.y * 0.5f,
-                    0);
-                
+
+                Debug.Log($"AFTER GET -> {marker.GetInstanceID()} Pos:{marker.transform.position}");
+
+                marker.transform.position = new Vector3(tile.WorldPosition.x + tile.TilemapMember.cellSize.x * 0.5f, tile.WorldPosition.y + tile.TilemapMember.cellSize.y * 0.5f, 0f);
+
+                Debug.Log($"AFTER MOVE -> {marker.GetInstanceID()} Pos:{marker.transform.position}");
+
                 HighlightOptions(marker, tile);
             }
         }
+
         private void HandleOnTileClicked(Vector3 _)
         {
+            Debug.Log("Tile Clicked");
+
             HideHighlightTiles();
         }
 
         private void HideHighlightTiles()
         {
+            Debug.Log($"HideHighlightTiles ({_actionTileMarkers.Count})");
+
             foreach (var marker in _actionTileMarkers.Where(m => m.activeSelf))
             {
                 _actionTileMarkerPool.Release(marker);
@@ -82,21 +109,13 @@ namespace _Scripts.Tiles.TileGrid
 
         private void HighlightOptions(GameObject marker, LevelTile tile)
         {
-            if (!marker.TryGetComponent<SpriteRenderer>(out var spriteRenderer))
+            if (!marker.TryGetComponent(out SpriteRenderer spriteRenderer))
             {
-                Debug.LogWarning($"Marker {marker.name} no tiene SpriteRenderer");
+                Debug.LogError("SpriteRenderer no encontrado");
                 return;
             }
 
-            spriteRenderer.color = tile.Occupant is IAIOccupant ? OccupantColor  :
-                tile.TileInteractor != null ? InteractorColor :
-                DefaultColor;
-        }
-        
-        private void OnDisable()
-        {
-            mediator.OnMovementTilesSet -= HighlightMovementTiles;
-            mediator.OnTileClicked -= HandleOnTileClicked;
+            spriteRenderer.color = tile.Occupant is IAIOccupant ? OccupantColor : tile.TileInteractor != null ? InteractorColor : DefaultColor;
         }
     }
 }
